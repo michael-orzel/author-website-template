819cff3f143d7a087af82bf1fdf3c015c2d4e435
Michael Orzel <56739010+michael-orzel@users.noreply.github.com>
2021-01-19 13:32:12 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const bars = document.querySelector('.nav-bars');
+const overlay = document.querySelector('.overlay');
+
+function toggleDropdown() {
+  const navDropdown = document.querySelector('.nav-dropdown');
+  if (navDropdown.style.display === "block") {
+    navDropdown.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    navDropdown.style.display = "block";
+    overlay.style.display = "block";
+  }
+}
+
+function removeOverlay() {
+  const navDropdown = document.querySelector('.nav-dropdown');
+  navDropdown.style.display = "none";
+  overlay.style.display = "none";
+}
+
+bars.addEventListener('click', toggleDropdown);
+overlay.addEventListener('click', removeOverlay);
